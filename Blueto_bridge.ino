917deb4bf53b5e277aa393763068809792eae476
kennet-one <kennet1one@gmail.com>
2024-09-12 18:13:08 +0200

    stability bug fix

--- Blueto_bridge.ino
+++ Blueto_bridge.ino
@@ -7,36 +7,17 @@
 
 #define   MESH_PREFIX     "kennet"
 #define   MESH_PASSWORD   "kennet123"
+#define   MESH_PORT       5555
 
+Scheduler userScheduler;
 painlessMesh  mesh;
 BluetoothSerial SerialBT;
 
-// bool garlandS;
-
-// bool redledP;
-// int modeR;
-
-// unsigned long pM = 0; // Змінна для зберігання часу останньої відправки
-// const int interval = 10; // Інтервал затримки в мілісекундах
 
 void callback(esp_spp_cb_event_t event, esp_spp_cb_param_t *param){
   if(event == ESP_SPP_SRV_OPEN_EVT){
     Serial.println("Client Connected");
     SerialBT.print("hello");
-
-    // unsigned long cM = millis();
-
-    // if (cM - pM >= interval) {
-    //   pM = cM; // Зберігання часу останньої відправки
-      
-    //   mesh.sendSingle(2224853816,"garland_echo");
-    //   pM = millis(); 
-    //   mesh.sendSingle(624315197,"red_led_echo");
-    //   pM = millis();
-    //   mesh.sendSingle(635035530,"bedside_echo");
-    //   pM = millis();
-    //   mesh.sendSingle(985208077,"sens_echo");
-    // }
   }
 
  
@@ -56,7 +37,7 @@ void setup() {
   Serial.begin(115200);
   //Serial.print("start");
 
-  mesh.init( MESH_PREFIX, MESH_PASSWORD );
+  mesh.init( MESH_PREFIX, MESH_PASSWORD, &userScheduler, MESH_PORT );
   mesh.onReceive(&receivedCallback);
 
   SerialBT.register_callback(callback);
