b15c9915c90a8591da32a1c0d335b4f1b51c4d04
kennet-one <kennet1one@gmail.com>
2024-09-27 19:32:51 +0200

    bug fix

--- Blueto_bridge.ino
+++ Blueto_bridge.ino
@@ -3,7 +3,7 @@
 //
 #include "painlessMesh.h"
 #include "BluetoothSerial.h"
-#include <ArduinoJson.h>
+
 
 #define   MESH_PREFIX     "kennet"
 #define   MESH_PASSWORD   "kennet123"
@@ -26,92 +26,93 @@ void callback(esp_spp_cb_event_t event, esp_spp_cb_param_t *param){
     Serial.println("Client Connected");
     SerialBT.print("hello");
   }
-  if(event == ESP_SPP_CLOSE_EVT ){
+
+ 
+  if(event == ESP_SPP_CLOSE_EVT ){            //ребут при дісконекті
     Serial.println("Client disconnected");
     ESP.restart();
   }
 }
 
 void receivedCallback( uint32_t from, String &msg ) {
-  SerialBT.print(msg);
+  String str1 = msg.c_str();
+  SerialBT.print(str1);
+}
+
+String formatNodeList(String json) {
+  String formattedList = "nodes: ";
+  int start = 0;
+  int end = 0;
+  bool firstNode = true; // Прапор для додавання коми між нодами
+
+  // Проходимо по JSON-рядку, щоб витягти ID нод
+  while ((start = json.indexOf("\"nodeId\":", end)) != -1) {
+    start += 9; // Пропускаємо "nodeId":
+    end = json.indexOf(",", start); // Знаходимо кінець ID або кінець об'єкта
+
+    // Якщо не знайшли кому, шукаємо кінець об'єкта
+    if (end == -1) {
+      end = json.indexOf("}", start);
+    }
+
+    // Витягуємо ID ноди
+    String nodeId = json.substring(start, end);
+    nodeId.trim();
+    nodeId.replace("}", "");
+    nodeId.replace("]", "");
+
+    // Додаємо ID ноди у форматований список
+    if (!firstNode) {
+      formattedList += ","; // Додаємо розділювач між елементами
+    }
+    formattedList += nodeId;
+    firstNode = false;
+  }
+
+//  formattedList += "]"; // Закриваємо форматований список
+  return formattedList;
 }
 
 void setup() {
   Serial.begin(115200);
+  //Serial.print("start");
 
   mesh.init( MESH_PREFIX, MESH_PASSWORD, &userScheduler, MESH_PORT );
   mesh.onReceive(&receivedCallback);
   mesh.onNewConnection(&newConnectionCallback);
 
   SerialBT.register_callback(callback);
-  SerialBT.begin("Kennet'MASH'");
 
-  // Створення задачі для обробки Bluetooth
-  xTaskCreatePinnedToCore(
-    bluetoothTask,   // Функція задачі
-    "BluetoothTask", // Ім'я задачі
-    4096,            // Розмір стеку в байтах
-    NULL,            // Параметри задачі
-    1,               // Пріоритет задачі
-    NULL,            // Вказівник на ідентифікатор задачі
-    1);              // Ядро процесора (0 або 1)
+  SerialBT.begin("Kennet'MASH'"); 
 }
+
 
 void loop() {
   mesh.update();
-}
 
-// Додавання функції formatNodeList
-String formatNodeList(String json) {
-  String formattedList = "nodes: ";
-  const size_t capacity = JSON_ARRAY_SIZE(10) + 10*JSON_OBJECT_SIZE(2);
-  DynamicJsonDocument doc(capacity);
+  if (SerialBT.available()) {
+    String str = SerialBT.readString();
+    str.trim();
 
-  DeserializationError error = deserializeJson(doc, json);
-  if (error) {
-    SerialBT.println("Failed to parse JSON");
-    return "";
-  }
-
-  JsonArray array = doc.as<JsonArray>();
-  for (JsonObject obj : array) {
-    uint32_t nodeId = obj["nodeId"];
-    formattedList += String(nodeId) + ",";
-  }
-
-  if (formattedList.endsWith(",")) {
-    formattedList.remove(formattedList.length() - 1);
-  }
-
-  return formattedList;
-}
-
-// Задача для обробки Bluetooth
-void bluetoothTask(void * parameter) {
-  const int bufferSize = 256;
-  char buffer[bufferSize];
-
-  for(;;) {
-    if (SerialBT.available()) {
-      int len = SerialBT.readBytesUntil('\n', buffer, bufferSize - 1);
-      buffer[len] = '\0';
-      String str = String(buffer);
-      str.trim();
-
-      if (str.length() == 0) {
-        // Порожнє повідомлення
-      } else if (str.equals("dbg1")) {
-        debugi = true;
-        String jsonNodeList = mesh.subConnectionJson();
-        String formattedNodeList = formatNodeList(jsonNodeList);
-        SerialBT.println(formattedNodeList);
-      } else if (str.equals("dbg0")) {
-        debugi = false;
-      } else {
-        mesh.sendBroadcast(str);
-      }
+    // Ігноруємо порожні повідомлення
+    if (str.length() == 0) {
+      return;
     }
-    taskYIELD();
 
+    // Перевірка на команди debug
+    if (str.equals("dbg1")) {
+      debugi = true;
+      String jsonNodeList = mesh.subConnectionJson();
+      // Форматуємо рядок у бажаний формат
+      String formattedNodeList = formatNodeList(jsonNodeList);
+
+      SerialBT.println(formattedNodeList);
+
+    } else if (str.equals("dbg0")) {
+      debugi = false;
+    } else {
+      // Відправляємо повідомлення в mesh-мережу
+      mesh.sendBroadcast(str);
+    }
   }
 }
